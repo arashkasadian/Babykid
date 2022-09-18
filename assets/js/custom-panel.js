b58cbc4699da4689c2f4785defc2c3a9e2e89968
arashkasadian <arashkasadian0@gmail.com>
2022-09-18 12:45:56 +0430

    whole-website v.1

--- assets/js/custom-panel.js
+++ assets/js/custom-panel.js
@@ -1,35 +1,50 @@
-$(document).ready(function() {
-    var app = new Mapp({
-      element: "#map",
-      presets: {
-        latlng: {
-          lat: 35.73249,
-          lng: 51.42268
-        },
-        zoom: 10
+$(document).ready(function () {
+  var app = new Mapp({
+    element: "#map",
+    presets: {
+      latlng: {
+        //موقعیتی که نقشه از آنجا شروع میشود , میتوان آنرا با موقعیتی که کاربر از قبل ثبت کرده جایگزین کرد
+        lat: 35.73249,
+        lng: 51.42268
       },
-    apiKey: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6Ijk2NTA5MWRmNjBiMTdlODlkNmNhMDE4YTg3YmE3MTJiNmQzNjM2YmY1NDcwNmQ5YWMxNWM0NDhhNzg1Y2U3ZDQ2ZDE4ODY1ZGE2MWUyNTNjIn0.eyJhdWQiOiIxOTQ5MiIsImp0aSI6Ijk2NTA5MWRmNjBiMTdlODlkNmNhMDE4YTg3YmE3MTJiNmQzNjM2YmY1NDcwNmQ5YWMxNWM0NDhhNzg1Y2U3ZDQ2ZDE4ODY1ZGE2MWUyNTNjIiwiaWF0IjoxNjYzNDI1NTgxLCJuYmYiOjE2NjM0MjU1ODEsImV4cCI6MTY2NjEwNzU4MSwic3ViIjoiIiwic2NvcGVzIjpbImJhc2ljIl19.XZYmYTsCNiZGaGisdx3rMVe4DnHNsFomtX0dU9WepTQzDnYxAYW9ApMSGDaacCOZoA2GzKlu6sDtL3XRbDQjIcsb1Nc_gqBehUfk86IcaaF0J57szS_gDWOJ-DfeIaaDATovQU3QVBXWF7QA-sDjIWQGo2AScTwEndhZ2hBcXrMfU1Gw3JZ2onm1FuFG0HIIU5LBXD9SKNR6PRb8D87ZB7NfihVu-y8wDpdxukG5TOBnHMu1vW6Xv2Sjzxhy-3r2jDqBt95xzfMOQ9W0XTrDEM9RQALN21rW-pZLjjXa9BPJ-4jzaMcyRw_i9jqprLrTszPcgdlBVvYq7zfPkZizqw'
-    });
+      zoom: 10
+    },
+    // لطفا در سایت map.ir ثبت نام کنید
+    //و برای پروژه کلید را دریافت کنید
+    apiKey: ''
+  });
 
-    app.addVectorLayers();
-    app.addFullscreen();
-    app.addGeolocation();
-    // Add in a crosshair for the map
-    var crosshairIcon = L.icon({
-      iconUrl: 'https://cloud.son.ir/index.php/s/qVUHj7HJSr1A7MK/download',
-      iconSize:     [20, 20], // size of the icon
-      iconAnchor:   [10, 10], // point of the icon which will correspond to marker's location
-    });
-    var crosshairMarker = new L.marker(app.map.getCenter(), {icon: crosshairIcon, clickable:false});
-    crosshairMarker.addTo(app.map);
+  app.addVectorLayers();
+  app.addFullscreen();
+  app.addGeolocation();
+  // Add in a crosshair for the map
+  var crosshairIcon = L.icon({
+    iconUrl: '../assets/images/loc-tag.png',
+    iconSize: [50, 50], // size of the icon
+    iconAnchor: [10, 10], // point of the icon which will correspond to marker's location
+  });
+  var crosshairMarker = new L.marker(app.map.getCenter(), { icon: crosshairIcon, clickable: false });
+  crosshairMarker.addTo(app.map);
 
-    // Move the crosshair to the center of the map when the user pans
-    app.map.on('move', function(e) {
-      crosshairMarker.setLatLng(app.map.getCenter());
-
-    });
-
-    crosshairMarker.on('move', function(event){
-      console.log(event.latlng)
+  // Move the crosshair to the center of the map when the user pans
+  app.map.on('move', function (e) {
+    crosshairMarker.setLatLng(app.map.getCenter());
+  });
+  crosshairMarker.on('click', function (e) {
+    app.findReverseGeocode({
+      state: {
+        latlng: {
+          lat: crosshairMarker._latlng.lat,
+          lng: crosshairMarker._latlng.lng,
+        },
+      },
+      after: function (data) {
+        document.getElementById('address').value=data.address;
+        document.getElementById('lat').value=crosshairMarker._latlng.lat;
+        document.getElementById('lng').value=crosshairMarker._latlng.lng;
+      }
     });
   });
+
+
+});
